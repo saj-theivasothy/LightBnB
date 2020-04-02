2a879f9fca549bec6e83a42c266907c875d73482
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-04-02 20:40:43 +0000

    add query to get reservations data from database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -71,8 +71,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  return pool.query(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE end_date < now()::date
+  AND reservations.guest_id = $1
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date   
+  LIMIT $2;
+  `, values)
+  .then(res => res.rows)
+  .catch(err => err.stack)
 }
 exports.getAllReservations = getAllReservations;
 
